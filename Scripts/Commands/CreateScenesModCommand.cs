5c1eb67a236646569786f96ba77c2a65955c266f
moorowl <moorowl@outlook.com>
2025-07-12 15:36:51 +0100

    added command to create scenes mod

---
+++ Scripts/Commands/CreateScenesModCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using HarmonyLib;
+using I2.Loc;
+using PugMod;
+
+// ReSharper disable InconsistentNaming
+
+namespace SceneBuilder.Commands {
+	[HarmonyPatch]
+	public static class CreateScenesModCommand {
+		private static readonly MemberInfo MiRenderText = typeof(ChatWindow).GetMembersChecked().FirstOrDefault(x => x.GetNameChecked() == "RenderText");
+		
+		[HarmonyPatch(typeof(ChatWindow), nameof(ChatWindow.Deactivate))]
+		[HarmonyPrefix]
+		[HarmonyPriority(Priority.First)]
+		private static void Deactivate(ChatWindow __instance, ref bool commit) {
+			var text = __instance.inputField.GetText();
+			if (!commit || !text.ToLowerInvariant().StartsWith("/createscenesmod"))
+				return;
+			
+			commit = false;
+			
+			var args = text.Split(" ");
+			if (args.Length < 2 || string.IsNullOrWhiteSpace(args[1])) {
+				API.Reflection.Invoke(MiRenderText, __instance, LocalizationManager.GetTranslation("SceneBuilder:ModNameError"));
+				return;
+			}
+
+			var modName = args[1].Trim();
+			
+			API.Reflection.Invoke(MiRenderText, __instance, string.Format(LocalizationManager.GetTranslation("SceneBuilder:ModCreated"), $"{Constants.InternalName}/Content/{modName}"));
+
+			Main.StructureRequestClientSystem.CreateScenesMod(modName);
+		}
+	}
+}
